cb7baff3c05162427d602dbda92962711b9ec197
Wei (Jack) Sun <jacksunwei@gmail.com>
2026-04-14 14:38:41 -0700

    feat: add tapsense cask

---
+++ Casks/tapsense.rb
@@ -0,0 +1,16 @@
+cask "tapsense" do
+  version "0.1.0"
+  sha256 :no_check
+
+  url "https://github.com/Jacksunwei/tapsense/releases/download/v#{version}/TapSense.app.zip"
+  name "TapSense"
+  desc "MacBook accelerometer-based gesture triggers"
+  homepage "https://github.com/Jacksunwei/tapsense"
+
+  app "TapSense.app"
+
+  zap trash: [
+    "~/Library/Application Support/TapSense",
+    "~/Library/Preferences/com.jacksunwei.tapsense.plist",
+  ]
+end
